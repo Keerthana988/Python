dae9802ec4b11c6e803691799a08e42f3a6fde75
Keerthana H V <keerthanahv22k@gmail.com>
2022-06-23 20:29:04 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("enter adjective : ")
+verb1 = input("enter verb1: ")
+verb2 = input("enter verb2 : ")
+
+
+
+madlib = f"This project is very {adj}! its makes me so excited {verb1}!, I'm enjoying this project {verb2}"
+
+print(madlib)
